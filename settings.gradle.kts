9360501f9bc3618aca88b1c3a44aa577499210e8
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-08-14 16:08:55 -0400

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SahilShah_COMP304_Hands-On_Test2_W24"
+include(":app")
+ 
